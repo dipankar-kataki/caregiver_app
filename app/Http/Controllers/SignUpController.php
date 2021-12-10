191f2ee605edbaea79ec07bc900303d9e23d1c9e
DipankarKataki <Dipankar.Kataki@ekodus.com>
2021-12-10 17:52:06 +0530

    signup controller, migration, model, api created

---
+++ app/Http/Controllers/SignUpController.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Signup;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
+
+class SignUpController extends Controller
+{
+    public function signup(Request $request){
+       
+        $validator = Validator::make($request->all(),
+            [
+                'firstname' => 'required',
+                'lastname' => 'required',
+                'email' => 'required|email',
+                'password' => 'required',
+            ],[
+                'firstname.required' => 'First Name is required',
+                'lastname.required' => 'Last Name is required',
+                'email.required' => 'Email is required',
+                'password.required' => 'Password is required',
+            ]);
+
+            if($validator->fails()){
+                return response()->json(['success' => 'false','error' => $validator->errors(), 'status' => 400]);
+            }else{
+                $create = Signup::create([
+                    'firstname' => $request->firstname,
+                    'lastname' => $request->lastname,
+                    'email' => $request->email,
+                    'password' => Hash::make($request->password)
+                ]);
+
+                if($create){
+                    return response()->json(['success' => 'true', 'message' => 'Signup successfull', 'status' => 201]);
+                }else{
+                    return response()->json(['success' => 'false', 'message' => 'Something went wrong. Signup failed', 'status' => 500]);
+                }
+            }
+    }
+}
