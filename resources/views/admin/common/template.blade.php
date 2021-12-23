a0eae9e2a6c78dc79ab87ca4a04e4ce14ab3ac08
DipankarKataki <Dipankar.Kataki@ekodus.com>
2021-12-23 16:42:28 +0530

    template.blade.php added for reference

---
+++ resources/views/admin/common/template.blade.php
@@ -0,0 +1,35 @@
+@extends('admin.common.main')
+
+@section('cunstomHeader')
+
+@endsection
+
+
+@section('title', 'Admin | Page Title')
+
+
+@section('main')
+    <div class="content-wrapper">
+
+        <div class="page-header">
+            <h3 class="page-title">
+                <span class="page-title-icon bg-gradient-primary text-white mr-2">
+                    <i class="mdi mdi-home"></i>
+                </span> Page Name
+            </h3>
+            <nav aria-label="breadcrumb">
+                <ul class="breadcrumb">
+                    <li class="breadcrumb-item active" aria-current="page">
+                        <span></span>Overview <i class="mdi mdi-alert-circle-outline icon-sm text-primary align-middle"></i>
+                    </li>
+                </ul>
+            </nav>
+        </div>
+        {{-- Main Content Starts --}}
+    </div>
+@endsection
+
+
+@section('cunstomJS')
+
+@endsection
