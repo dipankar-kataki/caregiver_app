5a7570c8d702ab700be31cf3c540e2078b176134
dipankar kataki <dipankar.kataki@ekodus.com>
2022-04-22 17:10:33 +0530

    job section inside admin agency

--- resources/views/admin/agency/job.blade.php
+++ resources/views/admin/agency/job.blade.php
@@ -28,25 +28,23 @@
 
                 <tbody>
                     @foreach($job_details as $key =>  $item)
-                        @foreach ($item->jobs as $key2 => $item2)
-                            <tr>
-                                <td>{{$item2->id}}</td>
-                                <td>{{$item->business_name}}</td>
-                                <td>{{$item2->job_title}}</td>
-                                <td>{{$item2->amount_per_hour}}</td>
-                                <td>{{$item2->created_at}}</td>
-                                
-                                @if ($item2->is_activate == 1)
-                                   <td class="text-success">Newly Posted</td> 
-                                @elseif($item2->is_activate == 2)
-                                    <td class="text-primary">On Going</td>
-                                @else
-                                    <td class="text-danger">Closed</td>
-                                @endif
-                                <td>NULL</td>
-                                <td><a href="{{route('admin.agency.view.profile', ['id' => Crypt::encrypt($item->id)])}}"  target="_blank" class="btn btn-sm btn-primary waves-effect width-md waves-light">View Profile</a></td>
-                            </tr>
-                        @endforeach
+                        <tr>
+                            <td>{{$key + 1}}</td>
+                            <td>{{$item->business_name}}</td>
+                            <td>{{$item->job_title}}</td>
+                            <td>{{$item->amount_per_hour}}</td>
+                            <td>{{$item->created_at}}</td>
+                            
+                            @if ($item->is_activate == 1)
+                                <td class="text-success">Newly Posted</td> 
+                            @elseif($item->is_activate == 2)
+                                <td class="text-primary">On Going</td>
+                            @else
+                                <td class="text-danger">Closed</td>
+                            @endif
+                            <td>NULL</td>
+                            <td><a href="{{route('admin.agency.view.profile', ['id' => Crypt::encrypt($item->id)])}}"  target="_blank" class="btn btn-sm btn-primary waves-effect width-md waves-light">View Profile</a></td>
+                        </tr>
                     @endforeach
                 </tbody>
             </table>
