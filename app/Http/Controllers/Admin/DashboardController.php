811f801e54fe030cbdbb77c32cf21e279a5a11d9
dipankar kataki <dipankar.kataki@ekodus.com>
2022-04-26 13:49:29 +0530

    revenue generated

--- app/Http/Controllers/Admin/DashboardController.php
+++ app/Http/Controllers/Admin/DashboardController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\AgencyPayments;
 use App\Models\JobByAgency;
 use App\Models\User;
 use Illuminate\Http\Request;
@@ -14,6 +15,7 @@ class DashboardController extends Controller
         $total_caregivers = User::where('role', 2)->where('is_user_approved', 1)->count();
         $total_agencies = User::where('role', 3)->where('is_user_approved', 1)->count();
         $total_jobs_posted = JobByAgency::where('is_activate', 1)->count();
-        return view('admin.dashboard')->with(['total_caregivers' => $total_caregivers, 'total_agencies' => $total_agencies, 'total_jobs_posted' => $total_jobs_posted]);
+        $total_agency_payments = AgencyPayments::where('payment_status', 'success')->sum('amount');
+        return view('admin.dashboard')->with(['total_caregivers' => $total_caregivers, 'total_agencies' => $total_agencies, 'total_jobs_posted' => $total_jobs_posted, 'total_agency_payments' => $total_agency_payments]);
     }
 }
