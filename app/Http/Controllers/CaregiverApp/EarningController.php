5d95c61c4553091a1875edf309c2e3d2e84deb54
dipankar kataki <dipankar.kataki@ekodus.com>
2022-05-02 14:20:12 +0530

    earning details

--- app/Http/Controllers/CaregiverApp/EarningController.php
+++ app/Http/Controllers/CaregiverApp/EarningController.php
@@ -14,19 +14,25 @@ class EarningController extends Controller
     public function getEarningDetails(){
         $earning_details = CaregiverPayment::with('job')->where('user_id', auth('sanctum')->user()->id)->get();
         $new_details = [];
-        foreach($earning_details as $key => $item){
-            $user_details = User::where('id', $item->job->user_id)->get();
-            foreach($user_details as $key2 => $item2){
-                $details = [
-                    'amount' => $item->amount,
-                    'agency' => $item2->business_name,
-                    'received_on' => $item->created_at->diffForHumans()
-                ];
-                array_push($new_details, $details);
-            }
-        }
+        if(! $earning_details->isEmpty()){
 
-        return $this->success('Earnings fetched successfully.', $new_details, 'null', 200);
+            foreach($earning_details as $key => $item){
+                $user_details = User::where('id', $item->job->user_id)->get();
+                foreach($user_details as $key2 => $item2){
+                    $details = [
+                        'amount' => $item->amount,
+                        'agency' => $item2->business_name,
+                        'received_on' => $item->created_at->diffForHumans()
+                    ];
+                    array_push($new_details, $details);
+                }
+            }
+    
+            return $this->success('Earnings fetched successfully.', $new_details, 'null', 200);
+        }else{
+            return $this->success('Earnings fetched successfully.', $new_details, 'null', 200);
+        }
+        
 
     }
 }
