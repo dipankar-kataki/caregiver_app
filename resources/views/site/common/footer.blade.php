fb6b84b9812e6ba874c62d819a2880b5c602613f
dipankar kataki <dipankar.kataki@ekodus.com>
2022-05-31 17:53:49 +0530

    site lock bage added

--- resources/views/site/common/footer.blade.php
+++ resources/views/site/common/footer.blade.php
@@ -4,7 +4,9 @@
             <p class="text-muted mb-0 py-md-3 text-md-start text-center"> &copy; 2022 | PeaceWorc | Designed & Developed By <a
                     href="https://ekodus.com/" target="_blank" class="link text-muted">Ekodus Technologies</a></p>
             <p class="text-muted mb-0 py-md-3 text-md-start text-center"> <a class="text-muted link" target="_blank" href="{{ route('site.privacy') }}">Privacy
-                    Policy</a> | <a class="text-muted link" target="_blank" href="{{ route('site.terms') }}">Terms & Conditions</a> </p>
+                    Policy</a> | <a class="text-muted link" target="_blank" href="{{ route('site.terms') }}">Terms & Conditions</a> 
+                    <a href="#" onclick="window.open('https://www.sitelock.com/verify.php?site=ekodusproject.tech','SiteLock','width=600,height=600,left=160,top=170');" ><img class="img-responsive" alt="SiteLock" title="SiteLock" src="https://shield.sitelock.com/shield/ekodusproject.tech" /></a>
+                </p>
         </div>
     </div>
 </footer>
