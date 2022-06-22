7ad0d3cd5717dc9b8a8ad81de885ba817e0cb991
dipankar kataki <dipankar.kataki@ekodus.com>
2022-06-22 12:08:32 +0530

    payment history agency

--- app/Http/Controllers/AgencyApp/PaymentHistoryController.php
+++ app/Http/Controllers/AgencyApp/PaymentHistoryController.php
@@ -11,16 +11,21 @@ class PaymentHistoryController extends Controller
 {   
     use ApiResponser;
     public function getPaymentHistory(){
-        $payment_history = AgencyPayments::with('job')->where('agency_id', auth('sanctum')->user()->id)->latest()->get();
-        $new_details = [];
-        foreach( $payment_history as $item){
-            $details = [
-                'amount' => $item->amount,
-                'job_title' => $item->job->job_title,
-                'paid_on' => $item->created_at->diffForHumans()
-            ];
-            array_push($new_details, $details);
+        try{
+            $payment_history = AgencyPayments::with('job')->where('agency_id', auth('sanctum')->user()->id)->latest()->get();
+            $new_details = [];
+            foreach( $payment_history as $item){
+                $details = [
+                    'amount' => $item->amount,
+                    'job_title' => $item->job->job_title,
+                    'paid_on' => $item->created_at->diffForHumans()
+                ];
+                array_push($new_details, $details);
+            }
+            return $this->success('Payment history fetched successfully. Working try', $new_details, 'null', 200);
+        }catch(\Exception $e){
+            return $this->error('Whoops! Something went wrong. Failed to fetch payment history. Please try after sometime', null, 'null', 500);
         }
-        return $this->success('Payment history fetched successfully.', $new_details, 'null', 200);
+        
     }
 }
